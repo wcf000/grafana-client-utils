5778a316d497c9903ebae3ec374364bbd4b00322
TechWithTy <tyrique1.daniel@gmail.com>
2025-05-04 20:34:13 -0600

    Resync

--- grafana/_tests/locustfile.py
+++
@@ -1,8 +0,0 @@
-from locust import HttpUser, task, between
-
-class QuickstartUser(HttpUser):
-    wait_time = between(1, 5)
-
-    @task
-    def hello_world(self):
-        self.client.get("/")
--- grafana/deploy_dashboards.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Sync provisioning files to Grafana
-rsync -avz --delete .grafana/provisioning/ /etc/grafana/provisioning/
-
-# Restart Grafana to apply changes
-systemctl restart grafana-server
